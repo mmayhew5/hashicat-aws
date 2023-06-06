002cc7b8dc879bc3cebb047d5de2838ba4afc397
Mike <mmayhew@chicagobooth.edu>
2023-06-06 12:32:20 -0500

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,6 @@
 module "s3-bucket" {
   source  = "app.terraform.io/mike-mayhew-org/s3-bucket/aws"
   version = "2.8.0"
-  bucket = "mikes-s3-bucket"
+  bucket = "mike-s3-bucket"
   acl    = "private"
 }
